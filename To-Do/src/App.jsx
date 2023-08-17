78547ab560b1f0def0670b2db0acfcc027e85c59
Shanehortonm <shanehortonm@gmail.com>
2023-08-17 04:57:59 -0400

    work on Todo list, form completed, start of todos function

--- To-Do/src/App.jsx
+++ To-Do/src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
-import './App.css'
+import './styles.css'
 
 function App() {
+  const [newItem, setNewItem] = useState("")
+  const [toDos, setToDos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setToDos([...toDos, { id: crypto.randomUUID(), title: newItem, completed: false},])
+  }
 
   return (
-    <h1>Hi!
-    </h1>
+  <>
+  <form onSubmit={handleSubmit} className='new-item-form'>
+    <div className='form-row'>
+      <label htmlFor="item">New Item</label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id='item' />
+    </div>
+    <button className='btn'>Add</button>
+  </form>
+  <h1 className='header'>ToDo List</h1>
+  <ul className='list'>
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item 1
+      </label>
+      <button className='btn btn-danger'>Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox" />
+        Item 2
+      </label>
+      <button className='btn btn-danger'>Delete</button>
+    </li>
+  </ul>
+  </>
+
   )
 }
 
