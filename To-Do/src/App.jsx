9852cab05168d1b1f0eff6d5ed0fe0c304d6d3fa
Shanehortonm <shanehortonm@gmail.com>
2023-08-18 22:09:34 -0400

    work on check box and delete btn

--- To-Do/src/App.jsx
+++ To-Do/src/App.jsx
@@ -3,12 +3,18 @@ import './styles.css'
 
 function App() {
   const [newItem, setNewItem] = useState("")
-  const [toDos, setToDos] = useState([])
+  const [todos, setTodos] = useState([])
 
   function handleSubmit(e) {
     e.preventDefault()
 
-    setToDos([...toDos, { id: crypto.randomUUID(), title: newItem, completed: false},])
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+
+    setNewItem("")
   }
 
   return (
@@ -22,20 +28,16 @@ function App() {
   </form>
   <h1 className='header'>ToDo List</h1>
   <ul className='list'>
-    <li>
+    {todos.map(todo => {
+      return (<li key={todo.id}>
       <label>
-        <input type="checkbox" />
-        Item 1
+        <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+        {todo.title}
       </label>
       <button className='btn btn-danger'>Delete</button>
-    </li>
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 2
-      </label>
-      <button className='btn btn-danger'>Delete</button>
-    </li>
+    </li>)
+    })}
+    
   </ul>
   </>
 
