d8fe3835316054733fa4bbcf6246b92de7ced58e
Shanehortonm <shanehortonm@gmail.com>
2023-08-18 23:10:50 -0400

    delete btn modification

--- To-Do/src/App.jsx
+++ To-Do/src/App.jsx
@@ -17,6 +17,22 @@ function App() {
     setNewItem("")
   }
 
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return {...todo, completed}
+        }
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
   <>
   <form onSubmit={handleSubmit} className='new-item-form'>
@@ -28,13 +44,14 @@ function App() {
   </form>
   <h1 className='header'>ToDo List</h1>
   <ul className='list'>
+    {todos.length === 0 && "No Todos"}
     {todos.map(todo => {
       return (<li key={todo.id}>
       <label>
         <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
         {todo.title}
       </label>
-      <button className='btn btn-danger'>Delete</button>
+      <button onClick={() => deleteTodo(todo.id)} className='btn btn-danger'>Delete</button>
     </li>)
     })}
     
